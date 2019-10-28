210ec3ca59e92809409eee6abcc7546395d8ed69
Melissa Knapp <melknapp@gmail.com>
2019-10-28 15:22:41 -0400

    Dynamically added buttons from topics array to the top of the html page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,23 @@
 console.log("The Javascript is linked!");
+
+var topics = ["happy", "sad", "confused", "excited", "dubious", "relaxed", "sleepy", "angry", "annoyed", "joyful", "alert"];
+
+renderButtons();
+
+function renderButtons(){
+
+    //empty the buttons from the html
+    $("#buttonList").empty();
+    
+    //looping through the array of topics (emotions)
+    for(var i=0; i<topics.length; i++){
+
+        //dynamically generate buttons for each emotion in the array
+        var feeling = $("<button>");
+        feeling.addClass("giphy");
+        feeling.text(topics[i]);
+        feeling.val(topics[i]);
+        $("#buttonList").append(feeling);
+    }
+
+}
