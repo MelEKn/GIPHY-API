2bdb436289f22fc6e235bbf3500c72b6ca70db17
Melissa Knapp <melknapp@gmail.com>
2019-10-28 21:31:00 -0400

    Added on click for gifs so that if you click them they freeze and then re-animategit add -A!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,49 +22,53 @@ function renderButtons() {
 
 }
 
- // displayGifs function re-renders the HTML to display the appropriate content
-function displayGifs(){
+// displayGifs function re-renders the HTML to display the appropriate content
+function displayGifs() {
 
-console.log(this);
+    console.log(this);
 
-var emotion = $(this).val();
+    var emotion = $(this).val();
 
-console.log("emotion is " + emotion);
+    console.log("emotion is " + emotion);
 
-// var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + emotion + "&limit=10&apikey=bSgTMFxA6jmewtl9NzOw5IkASCqhn5sI";
+    // var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + emotion + "&limit=10&apikey=bSgTMFxA6jmewtl9NzOw5IkASCqhn5sI";
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + emotion + "&api_key=bSgTMFxA6jmewtl9NzOw5IkASCqhn5sI&limit=10";
-
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-
-    console.log(response);
-
-    //creates a div to hold the gif
-
-    var newDiv = $("<div>");
-
-    //runs a for loop to go through all 10 responses
-    for (var i = 0; i < 5; i++) {
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + emotion + "&api_key=bSgTMFxA6jmewtl9NzOw5IkASCqhn5sI&limit=10";
 
 
-        var gifName = response.data[i].title;
-        var gifAnimated = response.data[i].images.fixed_height.url;
-        var gifStill =response.data[i].images.fixed_height_still.url;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
 
-        console.log("gifAnimated is " + gifAnimated);
+        console.log(response);
 
-        newDiv.append("Title: " + gifName);
-        // newDiv.append("<img src='http://media0.giphy.com/media/" + response.data[i].id + "/200.gif'><br/>");
-        newDiv.append("<img src = '" + gifAnimated + "' data-still='" + gifStill + "' data-animate='" + gifAnimated + "' data-state='animated' class='gif'><br/>");
-    }
+        //creates a div to hold the gif
 
-    $("#gifs-view").html(newDiv);
+        var newDiv = $("<div>");
 
-});
+        //runs a for loop to go through all 10 responses
+        for (var i = 0; i < 5; i++) {
+
+
+            var gifName = response.data[i].title;
+            var gifAnimated = response.data[i].images.fixed_height.url;
+            var gifStill = response.data[i].images.fixed_height_still.url;
+
+            console.log("gifAnimated is " + gifAnimated);
+
+            newDiv.append("Title: " + gifName);
+            // newDiv.append("<img src='http://media0.giphy.com/media/" + response.data[i].id + "/200.gif'><br/>");
+            //appending the html for the gif to the new div
+
+            newDiv.append("<img src = '" + gifAnimated + "' data-still='" + gifStill + "' data-animate='" + gifAnimated + "' data-state='animated' class='gif'><br/>");
+        }
+
+        $("#gifs-view").html(newDiv);
+
+    });
+
+    
 }
 
 // This function handles events where the Add a Giphy button is clicked
@@ -83,5 +87,23 @@ $("#add-giphy").on("click", function (event) {
     renderButtons();
 });
 
-     // Adding click event listeners to all elements with a class of "giphy"
-     $(document).on("click", ".giphy", displayGifs);
+// Adding click event listeners to all elements with a class of "giphy"
+$(document).on("click", ".giphy", displayGifs);
+
+$(document).on("click", ".gif", function () {
+    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    console.log("The gif click function was activated");
+    var state = $(this).attr("data-state");
+    console.log("state is " + state);
+    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    // Then, set the image's data-state to animate
+    // Else set src to the data-still value
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+
+});
